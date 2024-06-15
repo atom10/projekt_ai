14680564ad7977f9c813de11a3a475c9acff04d0
Kacper Stępień <kacper2007x48@gmail.com>
2024-06-15 11:12:33 +0200

    Repair running app on Windows

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 # Start the services in detached mode
-docker-compose up -d --build --remove-orphans
-
+docker-compose up -d
 # Attach to the python_app container
 docker attach python_app
-
 docker-compose stop
