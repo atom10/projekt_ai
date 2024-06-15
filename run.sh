0f8f3748a77add10657c6ea8b56a7532844be0ef
Konrad Wilusz <konrad.wilusz56@gmail.com>
2024-06-15 21:40:55 +0200

    update gitignore

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Start the services in detached mode
-docker-compose up -d
-# Attach to the python_app container
-docker attach python_app
-docker-compose stop
+
+docker-compose up --build --remove-orphans
